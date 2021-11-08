b4b796cc5a0d50f5fb65e5186407a381f5dbc1bb
Luis Huertas <hu3rtas@gmail.com>
2021-11-08 14:57:49 -0500

    [08112021] Testing Added

---
+++ src/helpers/helpers.js
@@ -0,0 +1,61 @@
+/**
+ *
+ *
+ * @returns {{parseVariableValue: (configInitialValue: string) => any, getEnvParsedVariable: (configInitialValue: string) => string, isObject: (potentialObj) => boolean, hasOwnProperty: (obj, property) => boolean, testEnvRegex: (testValue: string) => boolean}}
+ */
+function envSettingsHelpers() {
+  const helpers = {};
+  const regex = new RegExp("\\$\\{([^}\\s]*)\\}");
+
+  helpers.isObject = (potentialObj) => {
+    if (
+      typeof potentialObj === 'object' && 
+      potentialObj !== null && 
+      Object.keys(potentialObj).length !== 0) {
+      return true;
+    }
+    return false;
+  }
+
+  helpers.hasOwnProperty = (obj, property) => {
+    if (obj.hasOwnProperty(property)) {
+      return true;
+    }
+    return false;
+  }
+
+  helpers.testEnvRegex = (testValue) => {
+    if (regex.test(testValue)) {
+      return true;
+    }
+    return false;
+  }
+
+  helpers.getEnvParsedVariable = (configInitialValue) => {
+    const startIndex = configInitialValue.indexOf("${") + 2;
+    const endIndex = configInitialValue.indexOf("}");
+    const environmentKey = configInitialValue.substring(startIndex , endIndex - startIndex + 2 );
+    const environmentValue = process.env[environmentKey];
+    if(typeof environmentValue !== 'undefined'){
+      return configInitialValue.replace("${" + environmentKey + "}" , environmentValue);
+    }
+    return configInitialValue;
+  }
+
+  helpers.parseVariableValue = (configInitialValue) => {
+    if (helpers.testEnvRegex(configInitialValue)) {
+      return null;
+    }
+    if(configInitialValue == "true" || configInitialValue == "false"){
+      const isTrueSet = (configInitialValue === "true");
+      return isTrueSet;
+    }
+    else{
+      return configInitialValue;
+    }
+  }
+
+  return helpers;
+}
+
+module.exports = envSettingsHelpers;
---
+++ test/env.settings.test.js
@@ -0,0 +1,72 @@
+const EnvSettings = require('../src/env/EnvSettings.js');
+const helpers = require('../src/helpers/helpers.js');
+const path = require('path');
+
+describe('Correct parsing from a configuration file', () => {
+
+  const envSettings = new EnvSettings();
+  const envSettingHelpers = helpers();
+
+  it('Is an object not null', () => {
+    const testCorrectObj = { oneKey: 1, twoKey: 2};
+    const resultEvaluation = envSettingHelpers.isObject(testCorrectObj);
+    expect(resultEvaluation).toBeTruthy();
+    const testInCorrectObjNull = null;
+    const resultEvaluationIncorrectNull = envSettingHelpers.isObject(testInCorrectObjNull);
+    expect(resultEvaluationIncorrectNull).toBeFalsy();
+    const testInCorrectObjEmpty = {};
+    const resultEvaluationIncorrectEmpty = envSettingHelpers.isObject(testInCorrectObjEmpty);
+    expect(resultEvaluationIncorrectEmpty).toBeFalsy();
+  })
+
+  it('Is has an own property', () => {
+    const testCorrectObj = new Object();
+    testCorrectObj.prop = 'exists';
+    const resultEvaluation = envSettingHelpers.hasOwnProperty(testCorrectObj, 'prop');
+    expect(resultEvaluation).toBeTruthy();
+    const resultEvaluationIncorrect = envSettingHelpers.hasOwnProperty(testCorrectObj, 'propTwo');
+    expect(resultEvaluationIncorrect).toBeFalsy();
+  })
+
+  it('Matches Regex', () => {
+    const testCorrect = '${FOO_VALUE}';
+    const resultEvaluation = envSettingHelpers.testEnvRegex(testCorrect);
+    expect(resultEvaluation).toBeTruthy();
+    const testInCorrect = '${FOO_VALUE';
+    const resultEvaluationIncorrect = envSettingHelpers.testEnvRegex(testInCorrect);
+    expect(resultEvaluationIncorrect).toBeFalsy();
+  })
+
+  it('Success In Parsing Env', () => {
+    const testCorrect = '${FOR_TEST}';
+    const resultEvaluation = envSettingHelpers.getEnvParsedVariable(testCorrect);
+    expect(resultEvaluation).toMatch(/TESTING/);
+    expect(resultEvaluation).not.toMatch(/TESTING1/);
+    const testInCorrect = '${FOR_TEST_TEST}';
+    const resultEvaluationIncorrect = envSettingHelpers.getEnvParsedVariable(testInCorrect);
+    expect(resultEvaluationIncorrect).toBe('${FOR_TEST_TEST}');
+  })
+
+  it('Success In Parsing Variable Values', () => {
+    const testCorrectBooleanTrue = 'true';
+    const resultEvaluationBooleanTrue = envSettingHelpers.parseVariableValue(testCorrectBooleanTrue);
+    expect(resultEvaluationBooleanTrue).toBeTruthy();
+    const testCorrectBooleanFalse = 'false';
+    const resultEvaluationBooleanFalse = envSettingHelpers.parseVariableValue(testCorrectBooleanFalse);
+    expect(resultEvaluationBooleanFalse).toBeFalsy();
+    const testCorrectSame = 'same';
+    const resultEvaluationSame = envSettingHelpers.parseVariableValue(testCorrectSame);
+    expect(resultEvaluationSame).toBe(testCorrectSame);
+    const testNull = '${FOR_TEST}';
+    const resultEvaluationNull = envSettingHelpers.parseVariableValue(testNull);
+    expect(resultEvaluationNull).toBe(null);
+  })
+
+  it('Parse a whole JSON', () => {
+    const route = path.join(__dirname, './mocks/application.json');
+    const parsedJson = envSettings.loadJsonFile(route, 'utf8');
+    expect(parsedJson.foo).toBe('foo');
+    expect(parsedJson.nested.testing).toBe('TESTING');
+  })
+
+});
